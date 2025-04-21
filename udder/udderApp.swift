c8290851b157b1277f10c20255eb36d27476fba9
Mohamed Khaled <muhameddkhalid@gmail.com>
2025-04-21 20:54:47 +0200

    Initial Commit

---
+++ udder/udderApp.swift
@@ -0,0 +1,17 @@
+//
+//  udderApp.swift
+//  udder
+//
+//  Created by MacOS on 21/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct udderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
