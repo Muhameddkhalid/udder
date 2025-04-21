8c163ac362723f7a33ffbf99757e96a1d0a366e8
Mohamed Khaled <muhameddkhalid@gmail.com>
2025-04-21 21:16:33 +0200

    second update

--- udder/ContentView.swift
+++ udder/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Project Updated add milk branch")
+            Text("update")
         }
         .padding()
     }
